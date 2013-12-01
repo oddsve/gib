d2671dd9fc9a7d176ffc3b57d43492cd92cec189
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2013-12-01 01:03:54 +0100

    Starting gib and github service, almost working.

--- public/controllers.js
+++ public/controllers.js
@@ -1,4 +1,5 @@
 angular.module('gib.controllers', [])
+
 .controller('IndexController',
   ['$scope', 'Github', '$location',
   function ($scope, Github, $location) {
@@ -14,18 +15,23 @@ angular.module('gib.controllers', [])
 }])
 
 .controller('BoardController',
-  ['$scope', 'Github', '$routeParams',
-  function ($scope, Github, $routeParams) {
+  ['$scope', 'Gib', '$routeParams',
+  function ($scope, Gib, $routeParams) {
 
-  $scope.stations = [
-    {"name":"Produktkø", "issues":[{"title":"Feil 1"},{"title":"Bug l2"}]},
-    {"name":"I arbeid" ,"issues":[{"title":"tull 3"},{"title":"bug 4"}]},
-    {"name":"Ferdig", "issues":[{"title":"Feil 5"},{"title":"bug 6"}]}
-  ]
+  //$scope.stations = [
+    //{"name":"Produktkø", "issues":[{"title":"Feil 1"},{"title":"Bug l2"}]},
+    //{"name":"I arbeid" ,"issues":[{"title":"tull 3"},{"title":"bug 4"}]},
+    //{"name":"Ferdig", "issues":[{"title":"Feil 5"},{"title":"bug 6"}]}
+  //]
+  //
   $scope.repo =  $routeParams.repo;
 
-  Github.repos().then(function (repos) {
-    $scope.repos = repos;
+  Gib.createBoard('oddsve', 'gib').then(function (board) {
+    board = JSON.parse(board);
+    $scope.stations = board.stations.map(function (station) {
+      return { name: station };
+    });
   });
+
 }]);
 
